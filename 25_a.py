4e9dfad78eeccea66dc446c26351aba0067ee11d
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-03 04:00:00 +0600

    Finished day 1

---
+++ 25_a.py
@@ -0,0 +1,40 @@
+with open("25_a.txt", "r") as dataFile:
+    
+    line = dataFile.readline()
+
+    first = -1
+    second = -2
+    third = -3
+    curr = 0
+
+    while line != "":
+        line = line.strip()
+
+        if line == "":
+            if curr > first:
+                third = second
+                second = first
+                first = curr
+            elif curr > second and curr < first:
+                third = second
+                second = curr
+            elif curr > third and curr < second:
+                third = curr
+
+            curr = 0
+        else:
+            curr += int(line)
+        
+        line = dataFile.readline()
+
+    if curr > first:
+        third = second
+        second = first
+        first = curr
+    elif curr > second and curr < first:
+        third = second
+        second = curr
+    elif curr > third and curr < second:
+        third = curr
+
+    print(first + second + third)
