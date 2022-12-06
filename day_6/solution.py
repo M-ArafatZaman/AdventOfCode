df5360e7c1d76d1514bc7ec3e7f8fb1f11bd0d43
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-06 11:08:59 +0600

    part 1

--- day_6/solution.py
+++ day_6/solution.py
@@ -6,11 +6,27 @@ def parseInput(fName="input.in"):
     with open(fName, "r") as dataFile:
         data = dataFile.read().splitlines()
 
-    print(data)
+    return data[0]
 
+def stringIsUnique(string):
+    return len(set(list(string))) == len(string)
+
+def part1():
+    stream = parseInput("input.in")
+    buffer = ""
+    for i in range(len(stream)):
+        buffer += stream[i]
+        
+        # Check
+        if len(buffer) == 4 and stringIsUnique(buffer):
+            print(i+1)
+            break 
+        
+        while len(buffer) >= 4:
+            buffer = buffer[1:]
 
 def main():
-    parseInput("sample.in")
+    part1()
 
 
 if __name__ == "__main__":
