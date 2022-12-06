7e43f98e5c5fff609037eb73826b934fe3a49a5c
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-06 11:18:51 +0600

    Cleanup

--- day_6/solution.py
+++ day_6/solution.py
@@ -5,27 +5,22 @@ def parseInput(fName="input.in"):
     """
     with open(fName, "r") as dataFile:
         data = dataFile.read().splitlines()
-
     return data[0]
 
 def isStrUnique(string):
     return len(set(list(string))) == len(string)
 
 def solve(part=1):
-    if part == 1:
-        BUFFER_LEN = 4
-    else:
-        BUFFER_LEN = 14
+    if part == 1: BUFFER_LEN = 4
+    else: BUFFER_LEN = 14
 
     stream = parseInput("input.in")
     buffer = ""
     for i in range(len(stream)):
         buffer += stream[i]
-        
-        # Check
+        # Check if all chars are unique
         if len(buffer) == BUFFER_LEN and isStrUnique(buffer):
             return i+1
-        
         # Remove prev characters that exceed the buffer limit
         while len(buffer) >= BUFFER_LEN:
             buffer = buffer[1:]
