a420f245110712b577fa3192f08a944bcbdae4ff
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-09 10:57:13 +0600

    Day 9 initial commit

---
+++ day_9/solution.py
@@ -0,0 +1,14 @@
+
+def parseInput(fName="input.in"):
+    rdata = open(fName, "r").read().splitlines()
+
+    return rdata
+
+
+def solve():
+    #data = parseInput("sample.in")
+    data = parseInput("input.in")
+
+
+if __name__ == "__main__":
+    solve()
