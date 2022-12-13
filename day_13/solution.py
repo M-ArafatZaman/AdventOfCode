1c80babec26f075e2d9b1c474e214f0ebfdc8bc3
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-13 10:55:24 +0600

    Data 13 structure

---
+++ day_13/solution.py
@@ -0,0 +1,18 @@
+
+def parseInput(fName="input.in"):
+    rdata = open(fName, "r").readlines()
+
+    return rdata
+
+
+def solve():
+    data = parseInput("sample.in")
+
+def part1():
+    pass 
+
+def part2():
+    pass 
+
+if __name__ == "__main__":
+    solve()
